79839048126fdca9da1037d5c007b29018390ac6
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-04 02:17:53 +0600

    picture added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.Port||5000
+const cors = require('cors')
+const myData = require('./data.json');
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Muntasir Rifat')
 })
+
+app.get('/data', (req, res) => {
+    res.send(myData);
+  })
 
 app.listen(port, () => {
   console.log(`port is: ${port}`)
